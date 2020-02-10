a46f884fd15258532f4261978337546131352d7c
Matt Jeffery <matt.jeffery@arkansas.gov>
2020-02-10 14:22:00 -0600

    Remove unused esr views

--- web/src/views/esr/Home.vue
+++ web/src/views/esr/Home.vue
@@ -1,12 +1,6 @@
 <template>
     <div>
         <ESRMasthead />
-        <!--
-        <ESRTitle />
-        <ParticipantLogos />
-        <Intro year="2019" />
-        <HigherEducationPays />
-        -->
         <InstitutionSelection @selected="selectionInstitution"/>
     </div>
 </template>
@@ -14,24 +8,12 @@
 <script>
 import ESRMasthead from './ESRMasthead.vue';
 import InstitutionSelection from './InstitutionSelection.vue';
-/*
-import ESRTitle from './Title.vue';
-import ParticipantLogos from './ParticipantLogos.vue';
-import Intro from './Intro.vue';
-import HigherEducationPays from './HigherEducationPays.vue';
-*/
 
 export default {
     name: 'ESRHome',
     components: {
         ESRMasthead,
         InstitutionSelection,
-        /*
-        ESRTitle,
-        ParticipantLogos,
-        Intro,
-        HigherEducationPays,
-        */
     },
 
     data() {
@@ -42,7 +24,7 @@ export default {
 
     methods: {
         selectionInstitution(selection) {
-            this.$router.push({ name: 'esr-institution' });
+            this.$router.push({ name: 'esr.institution' });
         }
     },
 };
