91412a3486c06a12d0efc7046a4b637a71f075ab
Matt Jeffery <matt.jeffery@arkansas.gov>
2020-03-03 17:05:40 -0600

    Simple fix in isAdmin middleware

--- api/app/Http/Middleware/IsAdmin.php
+++ api/app/Http/Middleware/IsAdmin.php
@@ -6,11 +6,12 @@ use Closure;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Auth\AuthenticationException;
 //use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 /**
  * middleware to filter for admin users
  */
-class IsAdmin {
+class IsAdmin extends Middleware {
     public function handle($request, Closure $next) {
         if (Auth::user() &&  Auth::user()->isAdmin()) {
             return $next($request);
