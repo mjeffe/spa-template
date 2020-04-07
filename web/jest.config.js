b1d14e25b8cd2863d58db7c04c34b84aa8953661
Matt Jeffery <matt.jeffery@arkansas.gov>
2020-04-07 16:51:26 -0500

    Add config to run test coverage reports

--- web/jest.config.js
+++ web/jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    preset: '@vue/cli-plugin-unit-jest'
+    preset: '@vue/cli-plugin-unit-jest',
+    collectCoverage: false,
+    collectCoverageFrom: [
+        "src/**/*.{js,vue}",
+        "!**/node_modules/**"
+    ],
+    coverageReporters: [
+        /* "html", */
+        "text-summary"
+    ]
 };
